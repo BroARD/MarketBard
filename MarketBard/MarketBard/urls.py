87d475b3a4c92e522a205a25fa41149c36f14d77
BroARD <yvadis@mail.ru>
2024-06-22 18:27:06 +0700

    Fixed the picture of the products

--- MarketBard/MarketBard/urls.py
+++ MarketBard/MarketBard/urls.py
@@ -2,9 +2,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from products.views import index
+from django.conf.urls.static import static
+
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
