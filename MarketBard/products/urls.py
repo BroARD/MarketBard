b8ef13fad2b20d13ceb52e5fc89a1501536c63d6
BroARD <yvadis@mail.ru>
2024-06-23 17:19:54 +0700

    Add paginator with support category

--- MarketBard/products/urls.py
+++ MarketBard/products/urls.py
@@ -6,6 +6,7 @@ app_name = 'products'
 
 urlpatterns = [
     path('', products, name='index'),
-    path('<int:category_id>/', products, name='category')
-
+    path('<int:page>/', products, name='page'),
+    path('/category/<int:category_id>/', products, name='category'),
+    path('/category/<int:category_id>/<int:page>/', products, name='category_page'),
 ]
--- MarketBard/products/views.py
+++ MarketBard/products/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from products.models import Products, ProductCategory
 
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
     return render(request, 'products/index.html')
 
-def products(request, category_id=None):
+def products(request, category_id=None, page=1):
+    products = Products.objects.filter(category_id=category_id) if category_id else Products.objects.all()
 
-    if category_id:
-        category = ProductCategory.objects.get(id=category_id)
-        products = Products.objects.filter(category=category)
-    else:
-        products = Products.objects.all()
+    per_page = 3
+    paginator = Paginator(products, per_page=per_page)
+    products_paginator = paginator.page(page)
+
     context = {
-        'products': products,
+        'products': products_paginator,
         'category': ProductCategory.objects.all(),
+        'category_id': category_id,
     }
     return render(request, 'products/products.html', context)
